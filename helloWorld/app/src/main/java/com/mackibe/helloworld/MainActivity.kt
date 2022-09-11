75715e347cf959a7d5875ade20b8619bac9b8a20
/home/mackibe <ndichujames2030@gmail.com>
2022-09-11 22:30:27 +0300

    Added a Click Me button with an On click Event Listener

--- helloWorld/app/src/main/java/com/mackibe/helloworld/MainActivity.kt
+++ helloWorld/app/src/main/java/com/mackibe/helloworld/MainActivity.kt
@@ -2,10 +2,22 @@ package com.mackibe.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.clickMeBtn)
+        val textView = findViewById<TextView>(R.id.txtView)
+
+        btnClickMe.setOnClickListener {
+            btnClickMe.text = "Haha"
+            textView.text = "Hello World"
+
+        }
+
     }
 }
