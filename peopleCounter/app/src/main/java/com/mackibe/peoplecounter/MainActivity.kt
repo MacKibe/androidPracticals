c2d75fe7a73c07aef0976d4503d164871b1aaa8c
/home/mackibe <ndichujames2030@gmail.com>
2022-09-12 21:36:27 +0300

    created peopleCounter app

---
+++ peopleCounter/app/src/main/java/com/mackibe/peoplecounter/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.mackibe.peoplecounter
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mybtnCounter = findViewById<Button>(R.id.btnCounter)
+        val txtDisplay = findViewById<TextView>(R.id.countDisplayer)
+        var countNumber = 0
+
+        mybtnCounter.setOnClickListener {
+            countNumber += 1
+            txtDisplay.text = countNumber.toString()
+
+        }
+    }
+}
