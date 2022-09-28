d93827f2a6139adf1d7002a552774f8323ac4b14
mackibe <ndichujames2030@gmail.com>
2022-09-28 13:23:25 +0300

    added comments on the peopleCounter app

--- peopleCounter/app/src/main/java/com/mackibe/peoplecounter/MainActivity.kt
+++ peopleCounter/app/src/main/java/com/mackibe/peoplecounter/MainActivity.kt
@@ -1,21 +1,49 @@
+//
+// contains a set of related classes and method, simply the code of the app
+// it uniquely identifys your app on your device
 package com.mackibe.peoplecounter
-
+//
+// is the bundle library
 import android.os.Bundle
+//
+// is the widget button library
 import android.widget.Button
+//
+// is the widget textView library
 import android.widget.TextView
+//
+// is the library which helps in using the AppCompatActivity functionality
 import androidx.appcompat.app.AppCompatActivity
 
+//
+// the following app counts people when the button is tapped and display the total number of people
 class MainActivity : AppCompatActivity() {
+    //
+    // is onCreate function in the AppCompatActivity class is being override
     override fun onCreate(savedInstanceState: Bundle?) {
+        //
+        // allows the default execution of the AppCompatActivity
         super.onCreate(savedInstanceState)
+        //
+        // set the screen view of our application which is activity_main in layout folder
         setContentView(R.layout.activity_main)
-
+        //
+        // assigning a variable to button view id
         val mybtnCounter = findViewById<Button>(R.id.btnCounter)
+        //
+        // assigning a variables to button view id
         val txtDisplay = findViewById<TextView>(R.id.countDisplayer)
+        //
+        // assigned a variable countNumber a value of 0
         var countNumber = 0
-
+        //
+        // added an event listener when button is tapped
         mybtnCounter.setOnClickListener {
+            //
+            // increases the countNumber value when the user taps the button
             countNumber = countNumber + 1
+            //
+            // displays the final value
             txtDisplay.text = countNumber.toString()
         }
     }
