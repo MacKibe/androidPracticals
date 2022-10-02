e2d82619b10eebc96f5aef1c361912b9e6b4c9dc
mackibe <ndichujames2030@gmail.com>
2022-10-03 01:32:12 +0300

    modified the app; added button, textViews and editViews

--- Calculator/app/src/main/java/com/mackibe/simplecalc/MainActivity.kt
+++ Calculator/app/src/main/java/com/mackibe/simplecalc/MainActivity.kt
@@ -4,10 +4,30 @@ package com.mackibe.simplecalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    //
+    //
     override fun onCreate(savedInstanceState: Bundle?) {
+        //
+        // Allow the default execution of onCreate method
         super.onCreate(savedInstanceState)
+        //
+        // Set the main screen
         setContentView(R.layout.activity_main)
+
+        val addButton = findViewById<Button>(R.id.addButton)
+        val editText1 = findViewById<EditText>(R.id.editText1)
+        val editText2 = findViewById<EditText>(R.id.editText2)
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+
+        addButton.setOnClickListener {
+            val result = editText1.text.toString() + editText2.text.toString()
+            resultTextView.text = result
+        }
     }
+
 }
