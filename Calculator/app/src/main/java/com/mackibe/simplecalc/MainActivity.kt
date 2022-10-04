e260e04d52aad0913cb0029b2ef0e0fb568334f9
mackibe <ndichujames2030@gmail.com>
2022-10-04 10:56:04 +0300

    The App is running properly after the new changes and buttons added

--- Calculator/app/src/main/java/com/mackibe/simplecalc/MainActivity.kt
+++ Calculator/app/src/main/java/com/mackibe/simplecalc/MainActivity.kt
@@ -20,17 +20,26 @@ import android.widget.TextView
 // The following app is a simple calculator which adds two numbers.
 class MainActivity : AppCompatActivity() {
     //
-    // is onCreate function in the AppCompatActivity class is being override
+    // The onCreate function in the AppCompatActivity class is being overriden
     override fun onCreate(savedInstanceState: Bundle?) {
         //
         // Allow the default execution of the onCreate method
         super.onCreate(savedInstanceState)
         //
-        // Set the main screen.
+        // Display the Calculator screen.
         setContentView(R.layout.activity_main)
         //
         // Get addButton id and assign variable of addBtn.
         val addBtn = findViewById<Button>(R.id.addButton)
+        //
+        // Get divideBtn id and assign variable of divBtn.
+        val divBtn = findViewById<Button>(R.id.divideBtn)
+        //
+        // Get multiplicationBtn id and assign variable of divBtn.
+        val multiplyBtn = findViewById<Button>(R.id.multiplicationBtn)
+        //
+        // Get multiplicationBtn id and assign variable of divBtn.
+        val subtractBtn = findViewById<Button>(R.id.subtractionBtn)
         //
         // Get editText1 id and assign variable of num1.
         val num1 = findViewById<EditText>(R.id.editText1)
@@ -41,18 +50,70 @@ class MainActivity : AppCompatActivity() {
         // Get addResult id and assign variable of additionResults.
         val additionResults = findViewById<TextView>(R.id.addResult)
         //
-        // Convert the two values from EditText to integers.
-        val numA = num1.text.toString().toInt()
-        val numB = num2.text.toString().toInt()
+        // Get divideResult id and assign variable of divResults.
+        val divResults = findViewById<TextView>(R.id.divideResult)
         //
-        // Add an event listener to the addBtn button to take action when the button is tapped.
+        // Get divideResult id and assign variable of divResults.
+        val multiResults = findViewById<TextView>(R.id.multipicationResult)
+        //
+        // Get divideResult id and assign variable of divResults.
+        val subtractResults = findViewById<TextView>(R.id.subtractionResults)
+        //
+        // Add an event listener to the addBtn button to add number A and B when the button is tapped.
         addBtn.setOnClickListener {
+            //
+            // Convert the two values from text to numbers so that we can do calculations.
+            val numA = num1.text.toString().toInt()
+            val numB = num2.text.toString().toInt()
             //
             // Add the two values
             val sum = numA + numB
             //
-            // Display the Addition results of values A and B
+            // Display the Addition results of values A and B.
+            // Remember to convert the numeric sum to text for display.
             additionResults.text = sum.toString()
+        }
+        //
+        // Add an event listener to the divBtn button to take action when the button is tapped.
+        divBtn.setOnClickListener {
+            //
+            // Convert the two values from text to numbers so that we can do calculations.
+            val numA = num1.text.toString().toInt()
+            val numB = num2.text.toString().toInt()
+            //
+            // Divide the two values
+            val div = numA / numB
+            //
+            // Display the division of values A and B
+            divResults.text = div.toString()
+        }
+        //
+        // Add an event listener to the multiplyBtn button to take action when the button is tapped.
+        multiplyBtn.setOnClickListener {
+            //
+            // Convert the two values from text to numbers so that we can do calculations.
+            val numA = num1.text.toString().toInt()
+            val numB = num2.text.toString().toInt()
+            //
+            // multiply the two values
+            val div = numA * numB
+            //
+            // Display the multiplication of values A and B
+            multiResults.text = div.toString()
+        }
+        //
+        // Add an event listener to the subtractBtn button to take action when the button is tapped.
+        subtractBtn.setOnClickListener {
+            //
+            // Convert the two values from text to numbers so that we can do calculations.
+            val numA = num1.text.toString().toInt()
+            val numB = num2.text.toString().toInt()
+            //
+            // multiply the two values
+            val div = numA - numB
+            //
+            // Display the multiplication of values A and B
+            subtractResults.text = div.toString()
         }
     }
 }
