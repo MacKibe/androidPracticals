cce26592b5552a7f0750bc32adf7efcd4396b0c9
mackibe <ndichujames2030@gmail.com>
2022-10-04 12:05:52 +0300

    added the mainAcitivty.kt file from oneButton branch to main branch

--- Calculator/app/src/main/java/com/mackibe/simplecalc/MainActivity.kt
+++ Calculator/app/src/main/java/com/mackibe/simplecalc/MainActivity.kt
@@ -29,30 +29,6 @@ class MainActivity : AppCompatActivity() {
         // Display the Calculator screen.
         setContentView(R.layout.activity_main)
         //
-        // Getting the EditText element and giving a variable of numA
-        val numA = findViewById<EditText>(R.id.valueOfA)
-        //
-        // Getting the EditText element and giving a variable of numB
-        val numB = findViewById<EditText>(R.id.valueOfB)
-        //
-        // Getting the TextView element and giving a variable of addREsDisplay
-        val addResDisplay = findViewById<TextView>(R.id.additionResults)
-        //
-        // Getting the Button element and giving a variable of addBtn
-        val addBtn = findViewById<Button>(R.id.addButton)
-        //
-        // Listener to implement addition functionality when the button is tapped
-        addBtn.setOnClickListener {
-            //
-            // Get values from the EditText and converting them to integers before adding
-            var num1 = numA.text.toString().toInt()
-            var num2 = numB.text.toString().toInt()
-            //
-            // Getting the sum of the two numbers
-            var sum = num1 + num2
-            //
-            // Set the answer TextView text property to the result gotten
-            addResDisplay.text = sum.toString()
         // Get addButton id and assign variable of addBtn.
         val addBtn = findViewById<Button>(R.id.addButton)
         //
